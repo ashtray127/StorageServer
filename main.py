8a3b487ea20ed9778096aaf2f6db5e02de4d66ba
Brandon <66843207+goosemannn@users.noreply.github.com>
2023-01-18 15:44:04 +0000

    i dont know

--- main.py
+++ main.py
@@ -49,25 +49,18 @@ def icons(filename):
 @app.route('/view/<path:req_path>')
 def dir_listing(req_path):
     BASE_DIR = './storage'
-
-    # Joining the base and the requested path
     abs_path = os.path.join(BASE_DIR, req_path)
-
-    # Return 404 if path doesn't exist
     if not os.path.exists(abs_path):
         return abort(404)
 
-    # Check if path is a file and serve
     if os.path.isfile(abs_path):
         return send_file(abs_path)
 
-    # Show directory contents
     files = next(os.walk(abs_path))[1]
     folders = next(os.walk(abs_path))[2]
-    if req_path == '':
-        back = False 
-    else:
-        back = True
+    if req_path == '': back = False 
+    else: back = True
+    
     return render_template('files.html', files=files, folders=folders, back=back)
 
 @app.route('/upload', methods=['POST'])
